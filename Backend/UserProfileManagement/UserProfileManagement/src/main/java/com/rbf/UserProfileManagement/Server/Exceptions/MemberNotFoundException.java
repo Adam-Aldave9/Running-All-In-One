1686597de52d6d17329ad0cc3ef4345175ec1910
Adam-Aldave9 <adam.aldv9@gmail.com>
2024-07-23 13:10:33 -0400

    exception updated

--- Backend/UserProfileManagement/UserProfileManagement/src/main/java/com/rbf/UserProfileManagement/Server/Exceptions/MemberNotFoundException.java
+++ Backend/UserProfileManagement/UserProfileManagement/src/main/java/com/rbf/UserProfileManagement/Server/Exceptions/MemberNotFoundException.java
@@ -4,10 +4,10 @@ import java.util.UUID;
 
 public class MemberNotFoundException extends RuntimeException{
     public MemberNotFoundException(Integer id) {
-        super("Could not find employee " + id);
+        super("Could not find Member " + id);
     }
 
     public MemberNotFoundException(UUID id) {
-        super("Could not find employee " + id);
+        super("Could not find Member " + id);
     }
 }
